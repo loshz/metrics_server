bff79158fd8760d99926332d138e1af0e734064d
Dan Bond <danbond@protonmail.com>
2022-04-04 05:49:31 -0700

    store server
    Signed-off-by: Dan Bond <danbond@protonmail.com>

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,16 @@
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
 use std::thread;
 
 use tiny_http::{Method, Response, Server};
 
-/// A thread-safe growable array.
-#[derive(Clone)]
-pub struct MetricsServer(Arc<Mutex<Vec<u8>>>);
+/// A thread-safe datastore for serving metrics via a HTTP server.
+pub struct MetricsServer {
+    data: Arc<Mutex<Vec<u8>>>,
+    server: Arc<Server>,
+    stop: Arc<AtomicBool>,
+    thread: Option<thread::JoinHandle<()>>,
+}
 
 impl Default for MetricsServer {
     fn default() -> Self {
@@ -26,7 +31,12 @@ impl MetricsServer {
     /// let server = MetricsServer::new();
     /// ```
     pub fn new() -> Self {
-        MetricsServer(Arc::new(Mutex::new(Vec::new())))
+        MetricsServer {
+            data: Arc::new(Mutex::new(Vec::new())),
+            server: Arc::new(Server::http("localhost:8001").unwrap()),
+            stop: Arc::new(AtomicBool::new(false)),
+            thread: None,
+        }
     }
 
     /// Safely updates the data in a `MetricsServer` and returns the number of
@@ -40,12 +50,12 @@ impl MetricsServer {
     /// ```
     /// use metrics_server::MetricsServer;
     ///
-    /// let server = MetricsServer::new();
+    /// let mut server = MetricsServer::new();
     /// let bytes = server.update(Vec::from([1, 2, 3, 4]));
-    /// assert_eq!(bytes, 4);
+    /// assert_eq!(4, bytes);
     /// ```
     pub fn update(&self, data: Vec<u8>) -> usize {
-        let mut buf = self.0.lock().unwrap();
+        let mut buf = self.data.lock().unwrap();
         *buf = data;
         buf.as_slice().len()
     }
@@ -59,49 +69,44 @@ impl MetricsServer {
     /// ```
     /// use metrics_server::MetricsServer;
     ///
-    /// let server = MetricsServer::new();
+    /// let mut server = MetricsServer::new();
     /// server.serve("localhost:8001");
     /// ```
     ///
     /// # Panics
     ///
     /// Panics if given an invalid address.
-    pub fn serve(&self, addr: &str) {
+    pub fn serve(&mut self, _addr: &str) {
         // Create a new HTTP server and bind to the given address.
-        let server = Server::http(addr).unwrap();
+        //self.server = Some(Server::http(addr).unwrap());
 
         // Invoking clone on Arc produces a new Arc instance, which points to the
         // same allocation on the heap as the source Arc, while increasing a reference count.
-        let buf = Arc::clone(&self.0);
+        let buf = Arc::clone(&self.data);
+        let server = Arc::clone(&self.server);
+        let stop = Arc::clone(&self.stop);
 
         // Handle requests in a new thread so we can process in the background.
-        thread::spawn({
+        self.thread = Some(thread::spawn({
             move || {
-                loop {
-                    // Blocks until the next request is received.
-                    let req = match server.recv() {
-                        Ok(req) => req,
-                        Err(e) => {
-                            eprintln!("error: {}", e);
-                            continue;
-                        }
-                    };
+                // Blocks until the next request is received.
+                for req in server.incoming_requests() {
+                    // Check to see if we should stop handling requests.
+                    if stop.load(Ordering::Relaxed) {
+                        break;
+                    }
 
                     // Only respond to GET requests.
                     if req.method() != &Method::Get {
                         let res = Response::empty(405);
-                        if let Err(e) = req.respond(res) {
-                            eprintln!("{}", e);
-                        };
+                        let _ = req.respond(res);
                         continue;
                     }
 
                     // Only serve the /metrics path.
                     if req.url() != "/metrics" {
                         let res = Response::empty(404);
-                        if let Err(e) = req.respond(res) {
-                            eprintln!("{}", e);
-                        };
+                        let _ = req.respond(res);
                         continue;
                     }
 
@@ -109,10 +114,27 @@ impl MetricsServer {
                     let metrics = buf.lock().unwrap();
                     let res = Response::from_data(metrics.as_slice());
                     if let Err(e) = req.respond(res) {
-                        eprintln!("{}", e);
+                        eprintln!("metrics_server error: {}", e);
                     };
                 }
             }
-        });
+        }));
+    }
+}
+
+impl Drop for MetricsServer {
+    // TODO: should I really be doing this inside drop? It _could_ panic,
+    // so maybe a shutdown method would be better?
+    fn drop(&mut self) {
+        // Signal that we should stop handling requests and unblock the server.
+        self.stop.swap(true, Ordering::Relaxed);
+        self.server.unblock();
+
+        // Because join takes ownership of the thread, we need call the take method
+        // on the Option to move the value out of the Some variant and leave a None
+        // variant in its place.
+        if let Some(thread) = self.thread.take() {
+            thread.join().unwrap();
+        }
     }
 }
